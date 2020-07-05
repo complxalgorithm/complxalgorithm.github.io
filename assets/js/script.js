f9498e1a3ef95fae39bcd5d8491745096028ff9d
Stephen C. Sanders <scsandman6496@gmail.com>
2020-07-05 00:11:17 -0400

    main info will not fade in on load, added CTA section, additional small fixes

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// File: scripts.js
+// Author: Stephen C Sanders <https://stephensanders.me>
+// License: MIT
+
+// Animations
+$(function() {
+    // Title, nav buttons, and social buttons fade in
+    $( "#main-info" ).hide().fadeIn( 2000 );
+});
