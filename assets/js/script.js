226626327bc03f891b13a833d908494ee6571d2f
Stephen C. Sanders <scsandman6496@gmail.com>
2023-11-04 21:50:46 -0400

    changed method of fading in main info section from a javascript implementation to using css on the index file

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// File: script.js
-// Author: Stephen C Sanders <https://stephensanders.me>
-// License: MIT
-
-// Animations
-$(function() {
-    // Title, nav buttons, and social buttons fade in
-    $( "#main-info" ).hide().fadeIn( 2000 );
-});
